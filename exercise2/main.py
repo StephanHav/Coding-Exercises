a6f349e8e54e5788d8ab957efeed7e943ebd43f0
Stephan.Havermans <stephan.havermans@ext.esa.int>
2023-07-29 20:11:30 +0200

    Switching PCs

--- exercise2/main.py
+++ exercise2/main.py
@@ -1,6 +1,6 @@
 import os
 import json
-import chardet
+
 
 def read_json(input_file):
 
@@ -17,16 +17,9 @@ def read_json(input_file):
 
         print(data)
 
-def detect_file_encoding(file_path):
-    with open(file_path, 'rb') as f:
-        result = chardet.detect(f.read())
-    return result['encoding']
-
 def main():
 
-    encoding = detect_file_encoding('deps.json')
-    print(f"The encoding of deps.json is: {encoding}")
-    #read_json('deps.json')
+    read_json('exercise2/deps.json')
     
 
 if __name__ == '__main__':
